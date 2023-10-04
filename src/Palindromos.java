3564b7e68445eab06834058867551a3cf0343ab8
Raul <rfernr08@estudiantes.unileon.es>
2023-10-04 11:53:44 +0200

    version iterativa

---
+++ src/Palindromos.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Palindromos {
+    public static void main(String[] args){
+        Scanner stdinput = new Scanner(System.in);
+        String frase = stdinput.next();
+        frase.trim();
+        frase.toLowerCase();
+        char[] registro = new char[frase.length()];
+        boolean palindromo = true;
+        for(int i = 0, j = registro.length; i < registro.length; i++, j--){
+            registro[i] = frase.charAt(j);
+        }
+        for(int x = 0; x < registro.length; x++){
+            if(registro[x] != frase.charAt(x)){
+                palindromo = false;
+            }
+        }
+        if(palindromo == true){
+            System.out.println("La frase es palindroma");
+        }else {
+            System.out.println("La frase NO es palindroma");
+        }
+    }
+}
