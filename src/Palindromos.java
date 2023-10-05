e8979d9ceebf8b843feabad20434a162ae54578f
Raul <rfernr08@estudiantes.unileon.es>
2023-10-05 11:57:58 +0200

    optimizacion

--- src/Palindromos.java
+++ src/Palindromos.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+//import java.util.StringUtils;
 
 public class Palindromos {
     public static void main(String[] args){
         Scanner stdinput = new Scanner(System.in);
         String frase = stdinput.nextLine();
-        frase = frase.replaceAll(" +","");
-        frase.toLowerCase();
-        char[] registro = new char[frase.length()];
+        assert frase.length() > 0;
+        frase = frase.replaceAll(" +","").toLowerCase();
+        StringBuilder fraseInv = new StringBuilder();
+        char[] registro = fraseInv.append(frase).reverse().toString().toCharArray();
         boolean palindromo = true;
-        for(int i = 0, j = frase.length() -1; i < registro.length; i++, j--){
-            registro[i] = frase.charAt(j);
-        }
         for(int x = 0; x < registro.length; x++){
             if(registro[x] != frase.charAt(x)){
                 palindromo = false;
